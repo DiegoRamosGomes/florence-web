05ee4a3909e0a1366c959bb72e006689e8eff536
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2022-03-16 15:29:44 -0300

    feat - adicionado top tratamentos

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="title">{{ this.title }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/styles/variables";
+
+.title {
+  text-align: center;
+  color: $blue;
+  margin: 70px 0;
+  font-size: 50px;
+}
+</style>
--- src/components/Treatments/TopTreatments.vue
+++ src/components/Treatments/TopTreatments.vue
@@ -1,25 +1,38 @@
 <template>
-  <div class="component-top-treatments">
-    <MainCard action="#" title="Tratamento Facial" img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita, facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
-    </MainCard>
-    <MainCard action="#" title="Tratamento Facial" img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita, facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
-    </MainCard>
-    <MainCard action="#" title="Tratamento Facial" img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita, facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
-    </MainCard>
-    <MainCard action="#" title="Tratamento Facial" img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita, facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
-    </MainCard>
+  <div class="content">
+    <SectionTitle title="Top Tratamentos" />
+    <div class="component-top-treatments">
+      <MainCard action="#" title="Tratamento Facial"
+                img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita,
+        facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
+      </MainCard>
+      <MainCard action="#" title="Tratamento Facial"
+                img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita,
+        facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
+      </MainCard>
+      <MainCard action="#" title="Tratamento Facial"
+                img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita,
+        facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
+      </MainCard>
+      <MainCard action="#" title="Tratamento Facial"
+                img="https://www.giolaser.com.br/wp-content/uploads/2021/02/img-card-tratamento-facial.jpg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur autem doloribus eos expedita,
+        facilis, magnam maiores non praesentium provident, sequi tempora velit. Ad at harum, inventore nisi porro saepe.
+      </MainCard>
+    </div>
   </div>
 </template>
 
 <script>
 import MainCard from "@/components/Card/MainCard";
+import SectionTitle from "@/components/SectionTitle";
+
 export default {
   name: "TopTreatments",
-  components: {MainCard}
+  components: {SectionTitle, MainCard}
 }
 </script>
 
